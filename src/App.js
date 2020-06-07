420901c827fc2b8f650fd1d1c246864cabc68cc3
huangk24 <huangk24@uw.edu>
2020-06-06 17:34:50 -0700

    add firebase auth

--- src/App.js
+++ src/App.js
@@ -5,27 +5,113 @@ import {HashRouter, Route, Switch, Link, Redirect} from 'react-router-dom';
 import {HomePage} from "./HomePage";
 import {AboutPage} from "./AboutPage";
 import {PeoplePage} from "./PeoplePage";
+import firebase from 'firebase/app';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+
 
 class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {user: null}
+  }
+
+  //Configure FirebaseUI (inside the component, public class field)
+  uiConfig = {
+    //which sign in values should we support?
+    signInOptions: [
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID
+    ],
+    //for external sign-in methods, use popup instead of redirect
+    signInFlow: 'popup',
+  };
+
+  componentDidMount() {
+    //when I signed in or signed out
+    this.authUnSubFunction = firebase.auth().onAuthStateChanged((firebaseUser) => {
+      if(firebaseUser) { //if exists, then we logged in
+        console.log("Logged in as", firebaseUser.email);
+        this.setState({user: firebaseUser})
+      } else {
+        console.log("Logged out");
+        this.setState({user: null})
+      }
+    })
+  }
+
+  componentWillUnmount() {
+    this.authUnSubFunction();
+  }
+
+  handleSignOut = () => {
+    firebase.auth().signOut()
+  }
+
   render() {
+    let content = null;
+    if(!this.state.user) { //signed out
+      content = (
+        <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()} />
+      )
+    } 
+    else { //signed in
+      content = (
+        <div>
+          <div className="alert alert-success">
+            <h3>Logged in as {this.state.user.displayName}
+              <button className="btn btn-warning float-right" onClick={this.handleSignOut}>
+                Sign Out
+              </button>
+            </h3>
+          </div>
+          <div className="body">
+          <HashRouter basename={process.env.PUBLIC_URL+'/'}>
+            <NavBar />
+            <Header />
+            <main>
+              <Switch>
+                <Route exact path="/">
+                    <Redirect to="/home" />
+                </Route>
+                <Route path="/home" component={HomePage} />
+                <Route path="/about" component={AboutPage} />
+                <Route path="/people" component={PeoplePage}/>
+              </Switch>
+            </main>
+            <Footer/>
+          </HashRouter>
+          </div>
+        </div>
+      )
+    }
+
     return (
-      <div className="body">
-      <HashRouter basename={process.env.PUBLIC_URL+'/'}>
-        <NavBar />
-        <Header />
-        <main>
-          <Switch>
-            <Route exact path="/">
-                <Redirect to="/home" />
-            </Route>
-            <Route path="/home" component={HomePage} />
-            <Route path="/about" component={AboutPage} />
-            <Route path="/people" component={PeoplePage}/>
-          </Switch>
-        </main>
-        <Footer/>
-      </HashRouter>
+      <div className="container">
+        {/* Only included if first clause is true */}
+        {this.state.errorMessage &&
+          <p className="alert alert-danger">{this.state.errorMessage}</p>
+        }
+        {/* Show content based on user login state */}
+        {content}
       </div>
+      // <div className="body">
+      // <HashRouter basename={process.env.PUBLIC_URL+'/'}>
+      //   <NavBar />
+      //   <Header />
+      //   <main>
+      //     <Switch>
+      //       <Route exact path="/">
+      //           <Redirect to="/home" />
+      //       </Route>
+      //       <Route path="/home" component={HomePage} />
+      //       <Route path="/about" component={AboutPage} />
+      //       <Route path="/people" component={PeoplePage}/>
+      //     </Switch>
+      //   </main>
+      //   <Footer/>
+      // </HashRouter>
+      // </div>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAKk6OwJhjz1V3cCA0f3swiyyx2NUWGJ5E",
+  authDomain: "info-340-project-a291f.firebaseapp.com",
+  databaseURL: "https://info-340-project-a291f.firebaseio.com",
+  projectId: "info-340-project-a291f",
+  storageBucket: "info-340-project-a291f.appspot.com",
+  messagingSenderId: "576311843510",
+  appId: "1:576311843510:web:5cb07be268fa710c832417"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
